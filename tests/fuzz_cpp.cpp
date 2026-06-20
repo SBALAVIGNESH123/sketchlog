fb991da16a3d96701d326b012d01315ae4685319
Bala Vignesh S <sbalavignesh123@gmail.com>
2026-06-20 22:59:42 +0530

    fix: bound fuzzer parameters to prevent OOM limits

--- tests/fuzz_cpp.cpp
+++ tests/fuzz_cpp.cpp
@@ -18,10 +18,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
             // parameterized constructor
             double rel_acc;
             std::memcpy(&rel_acc, data + 1, sizeof(double));
-            uint8_t hll_p = data[9];
+            uint8_t hll_p = (data[9] % 15) + 4; // 4 to 18
             uint32_t cms_w, cms_d;
             std::memcpy(&cms_w, data + 10, sizeof(uint32_t));
             std::memcpy(&cms_d, data + 14, sizeof(uint32_t));
+            cms_w = (cms_w % 100000) + 1;
+            cms_d = (cms_d % 20) + 1;
             
             i += 18;
             if (!std::isnan(rel_acc) && !std::isinf(rel_acc) && rel_acc >= 0.0001 && rel_acc < 1.0) {
